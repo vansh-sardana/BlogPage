bb20ac25beea61cb87c3e2e797147e20a0fc5e74
Vansh Sardana <vanshsrdn25703@gmail.com>
2024-04-02 20:38:08 +0530

    updating names

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -22,6 +22,7 @@ const Pagination = () => {
           <div className='font-bold text-sm'>
             Page {currPg} of {totalPg}
           </div>
+          <footer>Copyright © 2021. All Right Reserved by CodeWell Blogs .</footer>
         </div>
       }
     </div>
